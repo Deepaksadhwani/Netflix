df99209cbd4f795d70802ca7eb700aaf7da6dabf
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2024-04-04 14:22:37 +0530

    added Sign In and Sign UP functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInUser, setIsSignInUser] = useState(true);
+
+  const toggleSignIn = () => {
+    setIsSignInUser(!isSignInUser);
+  };
+
   return (
     <div>
       <Header />
@@ -12,8 +18,15 @@ const Login = () => {
       />
       <form className=" px-30 absolute left-0 right-0 mx-auto my-36 flex w-3/12 flex-col items-center space-y-6 bg-black bg-opacity-80 p-12 pb-20">
         <h1 className=" w-full  py-5 text-3xl font-semibold text-white">
-          Sign In
+          {isSignInUser ? "Sign In" : "Sign Up"}
         </h1>
+        {!isSignInUser && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="m-2  w-full rounded-md bg-gray-700 p-3  placeholder-gray-200"
+          />
+        )}
         <input
           type="email"
           placeholder="Email Address"
@@ -24,11 +37,16 @@ const Login = () => {
           placeholder="Password"
           className="m-2  w-full rounded-md bg-gray-700 p-3  placeholder-gray-200"
         />
-        <button className="text-md m-4 w-full  rounded-md bg-[#e50914] p-4 font-semibold text-white">
+        <button className="text-md m-4 w-full cursor-pointer rounded-md bg-[#e50914] p-4 font-semibold text-white">
           Sign In
         </button>
-        <p className="font py-2 font-medium text-white">
-          New to Netflix? Sign Up Now
+        <p
+          onClick={toggleSignIn}
+          className="font cursor-pointer py-2 font-medium text-white"
+        >
+          {isSignInUser
+            ? "New to Netflix ? Sign Up Now"
+            : "Already registered? Sign Up Now."}
         </p>
       </form>
     </div>
